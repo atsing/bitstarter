c72608ece7ec858e03e354ded08de4e07de16b39
Ubuntu <ubuntu@ip-172-31-15-212.us-west-1.compute.internal>
2013-07-09 06:58:11 +0000

    web.js

--- web.js
+++ web.js
@@ -5,11 +5,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 var buffer = new Buffer(1000);
-var data = fs.readFileSync('/home/ubuntu/bitstarter/index.html', 'utf8');
+buffer.write(fs.readFileSync('/home/ubuntu/bitstarter/index.html', 'utf8'),"utf-8");
 
 
 app.get('/', function(request, response) {
-    response.send(buffer.toString(data));
+    response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
